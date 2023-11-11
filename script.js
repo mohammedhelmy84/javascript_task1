07dac4ba57d4baff416957b6a43e173d715006f4
mohammed helmy <egyman930@gamil.com>
2023-11-11 21:22:49 +0200

    create project

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+  function products()
+  {
+   var product1 = 100;
+   var product2 = 100;
+   var product3 = 100;
+   var product4 = 100;
+   var product5 = 100;
+   var product6 = 100;
+   var product7 = 100;
+   var product8 = 100;
+   var total = product1 + product2 + product3 + product4 + product5 + product6 + product7 + product8;
+   document.getElementById("demo").innerHTML = total;
+  }
+
